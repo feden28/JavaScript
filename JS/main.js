e861301e7d5ef32ededb19009c28b81e83dfac14
Federico Castillo <feden.28@gmail.com>
2022-06-08 00:23:05 -0300

    avances entrega final

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,5 @@
 console.log("Bienvenido a la primer entrega de JS");
-// let fin = false;
-// let nota = 0;
-// let numAlu = "";
-// let menuPpal = false;
-// let cambiaAlu = true;
+var aluSelecc
 let listaAlumnos = [];
 class alumno {
     constructor(apellido, nombre, notaTotal, cantNotas) {
@@ -23,6 +19,7 @@ class alumno {
 }
 
 //Arrowfunction
+/*
 const obtenerDatosJson = ()=> {
     fetch("data/data.json")
         .then((respuesta)=>{
@@ -52,20 +49,30 @@ mostrarHTML = (empleados) => {
     
     contenido.innerHTML = html;
 }
+const contenido = document.querySelector('#contenido');*/
 
 //Evento
-const contenido = document.querySelector('#contenido');
+const desplegaForm = document.querySelector('#desplegaform');
+
+desplegaForm.addEventListener('click', mostraFormAgrega)
 
 const formAgrega = document.querySelector('#formAgrega');
 formAgrega.addEventListener('submit', ingresaAlumno);
-formAgrega.addEventListener('submit', obtenerDatosJson)
+//formAgrega.addEventListener('submit', obtenerDatosJson)
 
 const listaAlu = document.querySelector('#listaAlumno');
 listaAlu.addEventListener('click', listaAlumno);
 
 const contenedorAlumnos = document.querySelector(".contenedor-alumnos");
 
+const listarNotas = document.querySelector('#listaNotas')
+listarNotas.addEventListener('click', listaParaNota);
+
+const formNota = document.querySelector('#agregarNota');
+formNota.addEventListener('submit', sumando);
+
 //Funciones
+
 function ingresaAlumno(evt) {
     evt.preventDefault()
     const valueApe = document.querySelector('#apellidoAlu').value
@@ -94,28 +101,13 @@ function ingresaAlumno(evt) {
     // console.log(listaAlumnos)
     formAgrega.reset()
     localStorage.setItem('listaAlumnos', JSON.stringify(listaAlumnos))
-    
-
-    /*Funcion agrega alumno
-    Controlar formulario vacio.
-     if (valueNom === '') {
-                alert("Ingrese el nombre del Alumno")
-                formAgrega.reset()
-                return;
-            } else if (valueApe === '') {
-                alert("Ingrese el apellido del Alumno")
-                formAgrega.reset()
-                return;
-            }
-            listaAlumnos.push(new alumno(document.getElementById("apellidoAlu").value, document.getElementById("nombreAlu").value, 0, 0));
-
-    }*/
+    ocultaFormAgrega()
 }
 
 function listaAlumno() {
+    ocultaFormNota()
     limpiaLista();
     listaAlumnos = JSON.parse(localStorage.getItem('listaAlumnos')) || [];
-
 
     for (const alumno of listaAlumnos) {
         const divAlumno = document.createElement('div');
@@ -143,6 +135,93 @@ function listaAlumno() {
     for (const alum of eliminaAlumno) {
         alum.addEventListener('click', borraAlumno);
     }
+}
+
+function listaParaNota() {
+    ocultaFormNota()
+    limpiaLista();
+    listaAlumnos = JSON.parse(localStorage.getItem('listaAlumnos')) || [];
+
+    for (const alumno of listaAlumnos) {
+        const divNota = document.createElement('div');
+        divNota.classList.add('contenedor-alumnos');
+
+        const titApellidoNombre = document.createElement('h4');
+        titApellidoNombre.textContent = (alumno.apellido + " " + alumno.nombre);
+
+        const btnSelecciona = document.createElement('button');
+        btnSelecciona.className = "seleccionAlumno";
+        btnSelecciona.innerText = "Seleccionar"
+
+        const notaAlumno = document.createElement('h5');
+        notaAlumno.textContent = ("Suma de sus notas: " + alumno.notaTotal);
+
+        divNota.appendChild(titApellidoNombre)
+        divNota.appendChild(notaAlumno)
+        divNota.appendChild(btnSelecciona)
+        divNota.dataset.alumnoID = alumno.id;
+        contenedorAlumnos.appendChild(divNota)
+    }
+    const agregarNota = document.getElementsByClassName('seleccionAlumno');
+
+
+    for (const alum of agregarNota) {
+        alum.addEventListener('click', identificaAlu);
+        //alum.addEventListener('click', mostraFormNota);
+    }
+}
+
+function identificaAlu(evt2) {
+    evt2.preventDefault()
+    aluSelecc = evt2.target.parentElement.dataset.alumnoID
+    mostraFormNota()
+    return aluSelecc;
+}
+
+function sumando(evt3, identificaAlu) {
+    evt3.preventDefault()
+
+    const idAlum = listaAlumnos.map(item => {
+
+        return {
+            id: `${item.id}`
+        }
+
+    });
+    console.log(idAlum)
+    console.log(listaAlumnos)
+    console.log(aluSelecc)
+
+    const o = idAlum.find(elemento => {
+
+        return elemento.id === aluSelecc;
+
+    });
+
+    const n = idAlum.indexOf(o);
+
+    console.log(n)
+}
+
+function mostraFormAgrega() {
+    limpiaLista()
+    ocultaFormNota()
+    document.getElementsByClassName("desplega-form")[0].style.display = "block";
+
+}
+
+function ocultaFormAgrega() {
+    document.getElementsByClassName("desplega-form")[0].style.display = "none";
+
+}
+
+function mostraFormNota() {
+    document.getElementsByClassName("form-nota")[0].style.display = "block";
+
+}
+
+function ocultaFormNota() {
+    document.getElementsByClassName("form-nota")[0].style.display = "none";
 
 }
 
@@ -167,7 +246,6 @@ function borraAlumno(evt1) {
     }).then((result) => {
         if (result.isConfirmed) {
             const idB = evt1.target.parentElement.dataset.alumnoID
-            console.log(idB);
             listaAlumnos = listaAlumnos.filter(alumno => alumno.id != idB)
             localStorage.clear()
             localStorage.setItem('listaAlumnos', JSON.stringify(listaAlumnos))
@@ -187,29 +265,10 @@ function borraAlumno(evt1) {
             )
         }
     })
-
-/*
-    console.log("EL BOTON FUNCIONA")
-
-    const idB = evt1.target.parentElement.dataset.alumnoID
-    console.log(idB);
-    listaAlumnos = listaAlumnos.filter(alumno => alumno.id != idB)
-    localStorage.clear()
-    localStorage.setItem('listaAlumnos', JSON.stringify(listaAlumnos))
-
-    limpiaLista();
-*/
-    /*Como eliminaba antes: 
-    for (let i = 0; i < listaAlumnos.length; i++) {
-        console.log(i + 1 + " - ")
-        listaAlumnos[i].listar();
-    }
-    let elim = Number(prompt("Indique el número del Alúmno que desea Borrar"))
-    listaAlumnos.splice(elim - 1, 1);*/
 }
 
 function mensajeError() {
-    const padre = document.querySelector("#formAgrega");
+    const padre = document.querySelector("#errorMensaje");
 
 
     const mensajeError = document.createElement('p')
@@ -231,221 +290,3 @@ function limpiaLista() {
     }
 
 }
-
-/*Código Anterior - 
-
-function opciones() {
-    while (cambiaAlu != false) {
-        seleccionaAlumno();
-    }
-    console.log("Para: ")
-    //listaAlumnos[numAlu].listar();
-    console.log("Seleccione que desea hacer:")
-    console.log("N - Cargar Nota")
-    //console.log("E - Borrar Nota")
-    console.log("C - Calcular Promedio")
-    console.log("S - Situación Alumno")
-    console.log("O - Cambiar Alumno")
-    console.log("ESC - Salir al Menú Anterior")
-
-    let opc = prompt("ingrese su opción");
-    opc = opc.toUpperCase();
-    console.log("Su opción es: " + opc);
-
-    switch (opc) {
-        case "N":
-            cargaNota();
-            break;
-        case "E":
-            borraNota();
-            break;
-        case "C":
-            calculaPromedio();
-            break;
-        case "A":
-            analizaAlumno();
-            break;
-        case "O":
-            cambiaAlu = true;
-            break;
-        case "ESC":
-            menuPpal = true;
-            break;
-        default:
-            break;
-
-    }
-
-}
-
-function seleccionaAlumno() {
-    for (let i = 0; i < listaAlumnos.length; i++) {
-        console.log(i + 1 + " - ")
-        listaAlumnos[i].listar();
-    }
-    cambiaAlu = false
-    numAlu = prompt("A qué alumno le cargará notas, ingrese el número según lista.");
-    numAlu = numAlu - 1;
-    return numAlu;
-}
-
-function cargaNota() {
-    do {
-        nota = prompt("Ingrese la Nota")
-        nota = parseFloat(nota);
-        if (nota > 10) {
-            alert("La nota no puede ser mayor a 10! Reingrese")
-        }
-    } while (nota > 10);
-
-    if (isNaN(nota)) {
-        alert("El valor ingresado no es un número")
-    } else if (nota < 0) {
-        alert("La nota no puede ser negativa")
-    } else {
-        alert("Nota Sumada al total!!");
-        console.log(listaAlumnos[numAlu].sumaNota())
-        console.log(listaAlumnos[numAlu])
-    }
-}
-
-function borraNota() {
-    let nota = prompt("Ingrese la Nota a BORRAR")
-    nota = parseFloat(nota);
-    console.log (sumaNota);
-    if (isNaN(nota)) {
-        alert("El valor ingresado no es un número")
-    } else if (nota < 0) {
-        alert("La nota no puede ser negativa")
-    } else if (sumaNota <= 0) {
-
-        alert("La suma de Notas es Cero, no puede eliminar")
-
-    } else if (sumaNota - nota < 0) {
-        alert("No puede Eliminar más nota de la que el alumno tiene.")
-    } else {
-        alert("Nota Borrada del total!!");
-        sumaNota = sumaNota - nota;
-        i = i-1;
-        console.log ("Suma de Notas Parcial " + sumaNota)
-    }
-}
-
-
-function calculaPromedio() {
-
-    console.log("La Nota Promedio del Alumno " + nomAlumno + " es:" + sumaNota / i)
-
-}
-
-function analizaAlumno() {
-    alert("Opción no disponible temporalmente.")
-}
-
-while (fin != true) {
-
-        console.log("Seleccione que desea hacer:")
-        console.log("A - Menú Alumnos")
-        console.log("B - Menú Notas")
-        console.log("ESC - Salir")
-        let opc2 = prompt();
-        opc2 = opc2.toUpperCase();
-        switch (opc2){
-            case "A":
-                while(menuPpal != true){
-                    alumnos();
-                }
-                menuPpal = false;
-                break;
-            case "B":
-                while(menuPpal != true){
-                    opciones();
-                }
-                menuPpal = false;
-                break;
-            case "ESC":
-                fin = true;
-                break;
-            default:
-                break;
-        }
-
-    console.log (fin);
-    if (fin!=true){
-
-    } else if (confirm("Desea terminar?")){
-        alert("Hasta Pronto")
-    } else {
-        fin = false;
-    }
-
-}
-
-
-
-function alumnos() {
-            console.log("I - Ingresar nuevo Alumno")
-            console.log("L - Listar Alumnos")
-            console.log("B - Borrar Alumno")
-            console.log("ESC - Menú Anterior")
-
-            let opc1 = prompt("ingrese su opción");
-            opc1 = opc1.toUpperCase();
-
-            switch (opc1){
-                case "I":
-                    listaAlumnos.push(new alumno (prompt("Ingrese el Apellido del Alumno"), prompt("Ingrese el Nombre del Alumno") , 0));
-                    listaAlumnos.sort((x,y) => {
-                        if (x.nombre > y.nombre) {
-                            return 1;
-                        }
-                        if (x.nombre <y.nombre) {
-                            return -1;
-                        }
-                        return 0;
-                    });
-                    listaAlumnos.sort((x,y) => {
-                        if (x.apellido > y.apellido) {
-                            return 1;
-                        }
-                        if (x.apellido <y.apellido) {
-                            return -1;
-                        }
-                        return 0;
-                    });
-                    break;
-                case "L":
-                        for (const alumno of listaAlumnos) {
-                            const divAlumno = document.createElement('div');
-                            divAlumno.classList.add('contenedor-alumnos');  
-
-                            const titApellidoNombre = document.createElement('h4');
-                            titApellidoNombre.textContent = (alumno.apellido + " " + alumno.nombre);
-
-                            const notaAlumno = document.createElement('h5');
-                            notaAlumno.textContent = ("Suma de sus notas: " + alumno.notaTotal);
-
-                            divAlumno.appendChild(titApellidoNombre)
-                            divAlumno.appendChild(notaAlumno)
-                            contenedorAlumnos.appendChild(divAlumno)
-
-                        }
-
-
-                    break;
-                case "B":
-                    for(let i = 0; i<listaAlumnos.length; i++) {
-                        console.log(i+1 + " - ")
-                        listaAlumnos[i].listar();
-                    }
-                    let elim = Number(prompt("Indique el número del Alúmno que desea Borrar"))
-                    listaAlumnos.splice(elim-1 , 1);
-                    break;
-                case "ESC":
-                    menuPpal = true;
-                    break;
-                default:
-                    break;
-            }
-}
-*/
